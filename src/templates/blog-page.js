6c489299df694ac03176c67e5143ec87992b6e41
Aliif Arief Maulana <63054324+aliifam@users.noreply.github.com>
2020-11-25 10:23:33 +0700

    aadssffg

--- src/templates/blog-page.js
+++ src/templates/blog-page.js
@@ -11,10 +11,7 @@ class BlogPage extends Component {
   render() {
     const { data } = this.props
     const { markdownRemark: post } = data
-    const { disqusConfig } = 
-    url: `${config.siteUrl+location.pathname}`,
-    identifier: post.id,
-    title: post.title,
+    const { disqusConfig } = this.props
   
 
     return (
