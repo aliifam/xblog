899e9a0bdd01504dfc3b454d4036ea5681e9159c
Aliif Arief Maulana <63054324+aliifam@users.noreply.github.com>
2020-11-25 12:41:25 +0700

    auth

--- src/templates/blog-page.js
+++ src/templates/blog-page.js
@@ -21,7 +21,7 @@ class BlogPage extends Component {
           keywords={post.frontmatter.title.split(" ")}
         />
         <div className="blog-wrapper">
-          <span>{`${post.author} - ${post.frontmatter.date} - ${post.timeToRead} min Read`}</span>
+          <span>{`${post.frontmatter.author} - ${post.frontmatter.date} - ${post.timeToRead} min Read`}</span>
           <h1 className="title">{post.frontmatter.title}</h1>
           <div dangerouslySetInnerHTML={{ __html: post.html }} />
            <Disqus config={disqusConfig} />
