623a4329f438a42369aafe8328fac283e9fd4995
Aliif Arief Maulana <63054324+aliifam@users.noreply.github.com>
2020-11-25 10:29:22 +0700

    qwret

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-disqus`,
       options: {
-        shortname: `your-disqus-shortname`
+        shortname: `praditautama`
       },
     },
     "gatsby-plugin-netlify-cms",
